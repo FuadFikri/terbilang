97cd41e63fcac5a5195168b8ad1e2b7b0baab845
gifff <gifarydhimas@gmail.com>
2016-10-25 14:51:40 +0700

    test the testcase

--- test/terbilangTest.php
+++ test/terbilangTest.php
@@ -6,7 +6,7 @@ class terbilangTest extends TestCase
   public function testProcessOne()
   {
     $a = new Terbilang();
-    $b = $a->process(5.710.666.123);
+    $b = $a->process(5710666123);
 
     $c = "lima milyar tujuh ratus sepuluh juta enam ratus enam puluh enam ribu seratus dua puluh tiga";
 
